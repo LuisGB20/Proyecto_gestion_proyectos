4a0fb73b6a9777c71b484b33fdbd40a026e5a331
MikeInsano <snowiestax72@gmail.com>
2024-01-27 09:40:27 +0100

    Bolon - Maquetado

--- Client/proyecto-gestion/src/App.jsx
+++ Client/proyecto-gestion/src/App.jsx
@@ -1,16 +1,20 @@
-import './App.css'
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Home from './pages/Home'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login/Login.jsx';
+import Register from './pages/Register/Register.jsx'
+import ResourcesAndAssets from "./pages/Resources and Assets/Resources and assets.jsx";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/Login' element={<Login/>}/>
+          <Route path='/Register' element={<Register/>}/>
+          <Route path='/ResourcesAndAssets' element={<ResourcesAndAssets/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
