577de5cc4a8daa4ad7f5ec9726e7f7290e2b0ec0
Luis_Gomez <luisalbertogomezbroca@gmail.com>
2024-01-27 21:35:16 -0500

    Se agrego panel de equipos y se reparo problema con la carpeta IMG

--- Client/proyecto-gestion/src/pages/equipos/TodosEquipos.jsx
+++ Client/proyecto-gestion/src/pages/equipos/TodosEquipos.jsx
@@ -10,13 +10,21 @@ function TodosEquipos() {
             <div className='flex'>
                 <SidebarAdmin />
                 <div className='w-full h-full'>
-                    <div className='bg-white my-3 w-full h-20 flex justify-between'>
-                        <h1 className='text-2xl font-semibold italic p-5'>Equipos</h1>
-                        <button className='font-medium text-center text-lg italic bg-gradient-to-r from-[#1E4C6A]  to-[#1B7FC5] p-2 mx-5 my-3 rounded-lg text-white'>Agregar nuevo equipo</button>
+                    <div className='w-full h-full'>
+                        <div className='bg-white my-3 w-full h-20 flex justify-between'>
+                            <h1 className='text-2xl font-semibold italic p-5'>Equipos</h1>
+                            <button className='font-medium text-center text-lg italic bg-gradient-to-r from-[#1E4C6A]  to-[#1B7FC5] p-2 mx-5 my-3 rounded-lg text-white'>Agregar nuevo equipo</button>
+                        </div>
                     </div>
-                </div>
-                <div className='grid grid-cols-3 '>
-                    <TarjetasEquipo/>
+                    <div className='grid grid-cols-3 gap-4 mx-5 overflow-auto h-[623px]'>
+                        <TarjetasEquipo />
+                        <TarjetasEquipo />
+                        <TarjetasEquipo />
+                        <TarjetasEquipo />
+                        <TarjetasEquipo />
+                        <TarjetasEquipo />
+                        <TarjetasEquipo />
+                    </div>
                 </div>
             </div>
         </div>
