b66ef2e4222c950dc25ac32b6540e57368997912
Luis_Gomez <luisalbertogomezbroca@gmail.com>
2024-01-31 07:40:56 -0500

    Correcciones front, login y registro

--- Client/proyecto-gestion/src/App.jsx
+++ Client/proyecto-gestion/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import jwtDecode from 'jwt-decode';
 import Home from '../src/pages/Home/Home'
 import Login from "./pages/Login/Login";
 import Register from './pages/Register/Register';
@@ -21,34 +22,54 @@ import PerfilMiembro from "./pages/perfilMiembro/PerfilMiembro";
 import Dashboard from "./pages/Dash/Dashboard";
 import DashboardEquipos from "./pages/DashEquipos/DashboardEquipos";
 import Recuperar from './pages/RecuperarContraseña/Recuperar'
+import {ProtectedRoutes} from './Routes/ProtectedRoutes.js'
 
 function App() {
+
+  const token = sessionStorage.getItem('token');
+  const decodedToken = jwtDecode(token);
+  console.log(decodedToken);
+
+  const isRoleValid = decodedToken.roles.includes('Programador') || decodedToken.roles.includes('Analista') || decodedToken.roles.includes('Diseñador');
+
+  const isJefe = decodedToken.roles.includes('Jefe');
+
+
   return (
     <>
       <BrowserRouter>
         <Routes>
+          {/* General */}
+          <Route path='/Profile' element={<Profile />} />
+          <Route path='/Politicas' element={<Politicas />} />
+          <Route path='/*' element={<NotFound />} />
+          <Route path='/RecuperarContraseña' element={<Recuperar />} />
           <Route path='/' element={<Home />} />
           <Route path='/Login' element={<Login />} />
           <Route path='/Register' element={<Register />} />
-          <Route path='/ResourcesAndAssets' element={<ResourcesAndAssets />} />
-          <Route path='/SolicitudActivos' element={<SolicitudActivos />} />
-          <Route path='/SolicitudRecursos' element={<SolicitudRecursos />} />
-          <Route path='/TableroMiembros' element={<TableroMiembros />} />
-          <Route path='/TodosEquipos' element={<TodosEquipos />} />
-          <Route path='/Profile' element={<Profile />} />
-          <Route path='/NuevoProyecto' element={<NuevoProyecto />} />
-          <Route path='/NuevoEquipo' element={<NuevoEquipo />} />
-          <Route path='/Proyecto' element={<Proyecto />} />
-          <Route path='/Equipo' element={<Equipo/>}/>
-          <Route path='/Miembro' element={<Miembro />} />
-          <Route path='/TodosProyectos' element={<TodosProyectos />} />
-          <Route path='/UsuarioMiembro' element={<UsuarioMiembro />} />
-          <Route path='/*' element={<NotFound />} />
-          <Route path='/Politicas' element={<Politicas />} />
-          <Route path='/PerfilMiembro' element={<PerfilMiembro />} />
-          <Route path='/Dashboard' element={<Dashboard/>} />
-          <Route path='/DashboardEquipos' element={<DashboardEquipos/>} />
-          <Route path='/RecuperarContraseña' element={<Recuperar/>}/>
+
+          {/* Miembros */}
+          <Route element={<ProtectedRoutes onAc/>}>
+            <Route path='/ResourcesAndAssets' element={<ResourcesAndAssets />} />
+            <Route path='/SolicitudActivos' element={<SolicitudActivos />} />
+            <Route path='/SolicitudRecursos' element={<SolicitudRecursos />} />
+            <Route path='/TableroMiembros' element={<TableroMiembros />} />
+            <Route path='/Dashboard' element={<Dashboard />} />
+            <Route path='/DashboardEquipos' element={<DashboardEquipos />} />
+          </Route>
+
+          {/* Jefe */}
+          <Route>
+            <Route path='/TodosEquipos' element={<TodosEquipos />} />
+            <Route path='/NuevoProyecto' element={<NuevoProyecto />} />
+            <Route path='/NuevoEquipo' element={<NuevoEquipo />} />
+            <Route path='/PerfilMiembro' element={<PerfilMiembro />} />
+            <Route path='/Proyecto' element={<Proyecto />} />
+            <Route path='/Equipo' element={<Equipo />} />
+            <Route path='/Miembro' element={<Miembro />} />
+            <Route path='/TodosProyectos' element={<TodosProyectos />} />
+            <Route path='/UsuarioMiembro' element={<UsuarioMiembro />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
--- Client/proyecto-gestion/src/components/SidebarMiDashboard.jsx
+++ Client/proyecto-gestion/src/components/SidebarMiDashboard.jsx
@@ -2,14 +2,17 @@
 import { Link } from 'react-router-dom'
 
 function SidebarMiDashboard() {
+    const cerrarSesion = () => {
+        sessionStorage.removeItem('token')
+    }
     return (
         <div className='bg-white w-64 h-auto border-2'>
             <ul className='my-10 mx-auto'>
                 <li className='mx-auto text-center my-10'><Link to='/Dashboard' className='font-medium text-center text-lg italic mx-auto hover:bg-gradient-to-r hover:from-[#1E4C6A]  hover:to-[#1B7FC5] p-4 rounded-lg hover:text-white'>Mi Dashboard</Link></li>
                 <li className='mx-auto text-center my-10'><Link to='/DashboardEquipos' className='font-medium text-center text-lg italic mx-auto hover:bg-gradient-to-r hover:from-[#1E4C6A]  hover:to-[#1B7FC5] p-4 rounded-lg hover:text-white'>Mi equipo</Link></li>
-                <li className='mx-auto text-center my-10'><Link className='font-medium text-center text-lg italic mx-auto hover:bg-gradient-to-r hover:from-[#1E4C6A]  hover:to-[#1B7FC5] p-4 rounded-lg hover:text-white'>Tablero de miembros</Link></li>
-                <li className='mx-auto text-center my-10'><Link className='font-medium text-center text-lg italic mx-auto hover:bg-gradient-to-r hover:from-[#1E4C6A]  hover:to-[#1B7FC5] p-4 rounded-lg hover:text-white' to="/Miembro">Recursos y activos</Link></li>
-                <li className='mx-auto text-center my-10'><Link className='font-medium text-center text-lg italic mx-auto hover:bg-gradient-to-r hover:from-[#1E4C6A]  hover:to-[#1B7FC5] p-4 rounded-lg hover:text-white' to="/">Cerrar sesión</Link></li>
+                <li className='mx-auto text-center my-10'><Link to='/TableroMiembros' className='font-medium text-center text-lg italic mx-auto hover:bg-gradient-to-r hover:from-[#1E4C6A]  hover:to-[#1B7FC5] p-4 rounded-lg hover:text-white'>Tablero de miembros</Link></li>
+                <li className='mx-auto text-center my-10'><Link to='/ResourcesAndAssets' className='font-medium text-center text-lg italic mx-auto hover:bg-gradient-to-r hover:from-[#1E4C6A]  hover:to-[#1B7FC5] p-4 rounded-lg hover:text-white'>Recursos y activos</Link></li>
+                <li className='mx-auto text-center my-10' onClick={cerrarSesion}><Link className='font-medium text-center text-lg italic mx-auto hover:bg-gradient-to-r hover:from-[#1E4C6A]  hover:to-[#1B7FC5] p-4 rounded-lg hover:text-white' to="/">Cerrar sesión</Link></li>
             </ul>
         </div>
     )
